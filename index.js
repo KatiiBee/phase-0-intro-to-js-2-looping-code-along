77c8f33be5adcdce2758a67460aed7624048aefa
Katie Demetriff <kdemetriff@gmail.com>
2022-09-17 11:44:12 -0400

    commit tree a89d5267051043edf0ba811e9e3ae9965832047b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "suprise";
+
+function writeCards(names, event) {
+    let messages=[];
+    for(let i=0; i<names.length; i++){
+        messages.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+let number =10;
+function countDown(number){
+    while (number> -1){
+        console.log(number--)
+    }
+    return number;
+
+}
